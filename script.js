a23daf6c29aca860e13d360cfa605e89a60c98b8
mutyatolanrewaju@gmail.com <mutyatolanrewaju@gmail.com>
2025-04-23 08:26:56 +0100

    bulb

---
+++ script.js
@@ -0,0 +1,13 @@
+function light(show){
+
+    let img;
+    if (show ==2) {
+       
+
+     pic = "realistic-light-bulb-isolated_1284-41741.jpg" ;
+    }
+    else{
+        pic = "realistic-lit-light-bulb-isolated_1284-41774.jpg";   
+     }
+     document.getElementById('bulb').src = pic;
+}
